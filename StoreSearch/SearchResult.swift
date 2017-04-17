25525da1af56f3b8f33e669a6d1eab790bda5028
frank0ch0a <ochoa.francisco@hotmail.com>
2017-04-17 11:50:59 -0500

    Added a search bar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Francisco Ochoa on 17/04/17.
+//  Copyright © 2017 fochoa. All rights reserved.
+//
+
+import Foundation
+
+
+class SearchResult {
+    
+    var name = ""
+    var artistName = ""
+}

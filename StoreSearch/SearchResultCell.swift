53e9d4df3c0ed20d96b91356f26463ee2d973f67
frank0ch0a <ochoa.francisco@hotmail.com>
2017-04-17 16:20:47 -0500

    Use custom cells for search results

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,28 @@
+//
+//  SearchResultCellTableViewCell.swift
+//  StoreSearch
+//
+//  Created by Francisco Ochoa on 17/04/17.
+//  Copyright © 2017 fochoa. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -14,17 +14,34 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     var searchResults: [SearchResult] = []
     var hasSearched = false
+    
+    struct TableViewCellIdentifiers {
+         static let searchResultCell = "SearchResultCell"
+         static let nothingFoundCell = "NothingFoundCell"
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0,right: 0)
+        
+        var cellNib = UINib(nibName: TableViewCellIdentifiers.searchResultCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.searchResultCell)
+        tableView.rowHeight = 80
+        
+        cellNib = UINib(nibName: TableViewCellIdentifiers.nothingFoundCell,
+                        bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.nothingFoundCell)
        
     }
 
    
-
+   
 }
+
 
 extension SearchViewController: UISearchBarDelegate {
     
@@ -71,29 +88,19 @@ extension SearchViewController:UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cellIdentifier = "SearchResultCell"
-        var cell: UITableViewCell! = tableView.dequeueReusableCell(
-            withIdentifier: cellIdentifier)
-        
-        if cell == nil {
-            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
-        }
-        
-        
-        
         if searchResults.count == 0 {
-            
-            cell.textLabel!.text = "(Nothing found)"
-            cell.detailTextLabel!.text = ""
-        }else {
-            let searchResult = searchResults[indexPath.row];
-            cell.textLabel!.text = searchResult.name
-            cell.detailTextLabel!.text = searchResult.artistName
+            return tableView.dequeueReusableCell(
+                withIdentifier: TableViewCellIdentifiers.nothingFoundCell,
+                for: indexPath)
+        } else {
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: TableViewCellIdentifiers.searchResultCell,
+                for: indexPath) as! SearchResultCell
+            let searchResult = searchResults[indexPath.row]
+            cell.nameLabel.text = searchResult.name
+            cell.artistNameLabel.text = searchResult.artistName
+            return cell
         }
-        
-       
-        return cell
-        
     }
 
     
